69b8f7fcec0e83ee421a8ed7e6e074172d4e3c4b
Raul Flores <rmfcortes@msn.com>
2023-02-15 17:35:02 -0600

    Firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,32 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging())
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ScreenTrackingService,
+    UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'waterdeliver-677f6',
+    appId: '1:482954319366:web:428c4f3b0c28b811d19587',
+    databaseURL: 'https://waterdeliver-677f6-default-rtdb.firebaseio.com',
+    storageBucket: 'waterdeliver-677f6.appspot.com',
+    apiKey: 'AIzaSyCajTifCatwpFo1TZT5JKwIh2YYS4IsLjM',
+    authDomain: 'waterdeliver-677f6.firebaseapp.com',
+    messagingSenderId: '482954319366',
+    measurementId: 'G-S2XN094JG9',
+  },
   production: true
 };
